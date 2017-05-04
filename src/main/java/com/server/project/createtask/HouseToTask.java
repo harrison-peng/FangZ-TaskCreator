0b1e972ef2bcdc09923385a2ac6977b5fc22b6da
Hao <hichyen1207@gmail.com>
2017-05-04 13:55:40 +0800

    fix not classifying by time

--- src/main/java/com/server/project/createtask/HouseToTask.java
+++ src/main/java/com/server/project/createtask/HouseToTask.java
@@ -34,53 +34,48 @@ public class HouseToTask {
 			String selectSQL = "select * from house where id=" + id;
 			ResultSet selectRS = selectST.executeQuery(selectSQL);
 			while (selectRS.next()) {
-				/**
-				 * task time classify : 0-> morning; 1-> afternoon; 2-> night;
-				 * 3-> midnight;
-				 */
 				List<TaskInfomation> taskInfoList = taskCreator.createTask(selectRS.getString("address"));
-				for (int i = 0; i <= 3; i++) {
-					int countRoad = 0;
-					for (TaskInfomation taskInfo : taskInfoList) {
-						String startGeometry = "ST_GeomFromText('POINT(" + taskInfo.getStartLng() + " "
-								+ taskInfo.getStartLat() + ")', 4326)";
-						String endGeometry = "ST_GeomFromText('POINT(" + taskInfo.getEndLng() + " "
-								+ taskInfo.getEndLat() + ")', 4326)";
-						String taskTime;
-						if (i == 0) {
-							System.out.println("start insert morning task");
-							taskTime = "morning";
-						} else if (i == 1) {
-							System.out.println("start insert afternoon task");
-							taskTime = "afternoon";
-						} else if (i == 2) {
-							System.out.println("start insert night task");
-							taskTime = "night";
-						} else {
-							System.out.println("start insert midnight task");
-							taskTime = "midnight";
-						}
+				// for (int i = 0; i <= 3; i++) {
+				int countRoad = 0;
+				for (TaskInfomation taskInfo : taskInfoList) {
+					String startGeometry = "ST_GeomFromText('POINT(" + taskInfo.getStartLng() + " "
+							+ taskInfo.getStartLat() + ")', 4326)";
+					String endGeometry = "ST_GeomFromText('POINT(" + taskInfo.getEndLng() + " " + taskInfo.getEndLat()
+							+ ")', 4326)";
+					// String taskTime;
+					// if (i == 0) {
+					// System.out.println("start insert morning task");
+					// taskTime = "morning";
+					// } else if (i == 1) {
+					// System.out.println("start insert afternoon task");
+					// taskTime = "afternoon";
+					// } else if (i == 2) {
+					// System.out.println("start insert night task");
+					// taskTime = "night";
+					// } else {
+					// System.out.println("start insert midnight task");
+					// taskTime = "midnight";
+					// }
 
-						String title = selectRS.getString("address");
-						if (taskInfo.getStartAddress().contains("巷")) {
-							int toLaneIndex = taskInfo.getStartAddress().indexOf("巷");
-							String lane = taskInfo.getStartAddress().substring(0, toLaneIndex + 1);
-							title = lane + "_" + taskTime;
-						} else {
-							countRoad += 1;
-							title = selectRS.getString("address") + "(" + countRoad + ")_" + taskTime;
-						}
-
-						String insertSQL = "insert into task (title, address, start_geometry, end_geometry, time, distance, duration, start_address, end_address) values ('"
-								+ title + "', '" + selectRS.getString("address") + "', " + startGeometry + ", "
-								+ endGeometry + ", '" + taskTime + "', '" + taskInfo.getDistance() + "', '"
-								+ taskInfo.getDuration() + "', '" + taskInfo.getStartAddress() + "', '"
-								+ taskInfo.getEndAddress() + "');";
-						System.out.println(insertSQL);
-
-						updateST.executeUpdate(insertSQL);
+					String title = selectRS.getString("address");
+					if (taskInfo.getStartAddress().contains("巷")) {
+						int toLaneIndex = taskInfo.getStartAddress().indexOf("巷");
+						String lane = taskInfo.getStartAddress().substring(0, toLaneIndex + 1);
+						title = lane;
+					} else {
+						countRoad += 1;
+						title = selectRS.getString("address") + "(" + countRoad + ")";
 					}
+
+					String insertSQL = "insert into task (title, address, start_geometry, end_geometry, distance, duration, start_address, end_address) values ('"
+							+ title + "', '" + selectRS.getString("address") + "', " + startGeometry + ", "
+							+ endGeometry + ", '" + taskInfo.getDistance() + "', '" + taskInfo.getDuration() + "', '"
+							+ taskInfo.getStartAddress() + "', '" + taskInfo.getEndAddress() + "');";
+					System.out.println(insertSQL);
+
+					updateST.executeUpdate(insertSQL);
 				}
+				// }
 			}
 			if (id > 1) {
 				String updateStartSQL = "update house set endNote='done' where id=" + (id - 1) + ";";
